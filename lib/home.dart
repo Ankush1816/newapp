b0498d4067f132ba65df34c798119968a27fbd62
SivaRamana-H-V <shivadevi2021@gmail.com>
2023-11-19 00:14:12 +0530

    Initial page completed

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:newapp/Screen/splash/splash_screen.dart';
 import 'package:newapp/constants.dart';
+import 'package:newapp/rout.dart';
 
 class EcommerceApp extends StatelessWidget {
   const EcommerceApp({Key? key}) : super(key: key);
@@ -7,16 +9,18 @@ class EcommerceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         fontFamily: 'Muli',
         textTheme: const TextTheme(
           bodyLarge: TextStyle(color: kTextColor),
           bodyMedium: TextStyle(color: kTextColor),
-
         ),
       ),
-      home: Placeholder(),
+      // home: const SplashScreen(),
+      initialRoute: SplashScreen.routeName,
+      routes: routs,
     );
   }
 }
---
+++ lib/rout.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:newapp/Screen/splash/splash_screen.dart';
+
+final Map<String, WidgetBuilder> routs = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+};
